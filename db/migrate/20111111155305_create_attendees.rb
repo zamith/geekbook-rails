12e82b7dc0fe488d325d08f8156e1aea42b4aa19
Zamith <zamith.28@gmail.com>
2011-11-11 16:37:10 +0000

    Heroku changes

--- db/migrate/20111111155305_create_attendees.rb
+++ db/migrate/20111111155305_create_attendees.rb
@@ -1,6 +1,10 @@
 class CreateAttendees < ActiveRecord::Migration
   def change
     create_table :attendees do |t|
+      t.string :name,           :limit => 64
+      t.string :avatar_filename, :limit => 128
+      t.integer :geek_votes, :default => 0
+      t.string :profile_url
 
       t.timestamps
     end
